8657728988537fde758432981f9499a70dda2694
NazifaPushon <mashiat.pushon@gmail.com>
2022-07-20 00:02:06 +0600

    besic binary tree

--- tree.cpp
+++ tree.cpp
@@ -16,6 +16,10 @@ struct Node{
 
 int main (){
     struct Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(4);
     return 0;
 }
 
