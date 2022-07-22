51df25f3213d3c4962fd1e9fdcafb7c2b73d7487
NazifaPushon <mashiat.pushon@gmail.com>
2022-07-22 22:46:27 +0600

    pre order travarsal

--- new_tree.cpp
+++ new_tree.cpp
@@ -14,11 +14,25 @@ Node* createNode(int data){
     newNode->left = newNode->right = nullptr;
     return newNode;
 }
+
+void pre_order(Node *node){
+    cout << node->data << endl;
+    if(node->left != NULL){
+        pre_order(node->left);
+    }if(node->right != NULL){
+        pre_order(node->right);
+    }
+}
+
+
 int main (){
 
     Node* root = createNode(1);
     root->left = createNode(2);
     root->right = createNode(3);
     root->left->left = createNode(4);
+    root->left->right = createNode(5);
+
+    pre_order(root);
     return 0;
 }
